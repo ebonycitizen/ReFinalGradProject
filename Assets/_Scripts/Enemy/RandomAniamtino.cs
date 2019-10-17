a7ef5cfc31ca1d240c763966677b2daa356d160b
huyup <irrsfunmaker@gmail.com>
2019-10-17 17:48:44 +0900

    add エネミーの配置を追加

---
+++ Assets/_Scripts/Enemy/RandomAniamtino.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAniamtino : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var randomStartTime=Random.value;
+
+        Invoke("StartAnimation", randomStartTime);
+    }
+
+    void StartAnimation()
+    {
+        animator.SetTrigger("Play");
+    }
+
+}
