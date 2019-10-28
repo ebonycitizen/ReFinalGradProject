c063702c5eee1f20608ff4943b1452bd407dfa0e
ebonyCitizen <coco_coco87@hotmail.com>
2019-10-28 21:25:48 +0900

    crystal model + jump state

--- Assets/State/JumpState.cs
+++ Assets/State/JumpState.cs
@@ -2,27 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 using IceMilkTea.Core;
+using DG.Tweening;
 
 public partial class OrcaState
 {
     private class JumpState : ImtStateMachine<OrcaState>.State
     {
+        private Transform orca;
+        Sequence s;
+        Sequence s2;
+
+        private void Move()
+        {
+            var direction = Quaternion.Euler(Context.idleTarget.localEulerAngles) * Context.cameraRig.transform.forward;
+
+            var targetPos = direction * 7;
+
+            orca.localPosition = Vector3.Lerp(orca.localPosition, targetPos, Time.fixedDeltaTime * 2f);
+        }
+
+        private void Jump(Vector3 pos)
+        {
+            s = DOTween.Sequence();
+            s.Append(orca.DOLocalMoveY(orca.localPosition.y + 6, 1).SetEase(Ease.InOutQuad))
+                .Append(orca.DOLocalMoveY(orca.localPosition.y, 1).SetEase(Ease.InQuad))
+                .AppendCallback(()=> stateMachine.SendEvent((int)StateEventId.Idle));
+
+            s.Play();
+        }
+
+        private void Rotate()
+        {
+            s2 = DOTween.Sequence();
+            s2.Append(orca.DOBlendableLocalRotateBy(new Vector3(-45, 0, 0), 0.5f).SetEase(Ease.InOutQuad))
+                .Append(orca.DOBlendableLocalRotateBy(new Vector3(90, 0, 0), 1f).SetEase(Ease.InOutQuad))
+                .Append(orca.DOBlendableLocalRotateBy(new Vector3(-45, 0, 0), 1))
+                ;
+                
+            s2.Play();
+        }
+
         protected internal override void Enter()
         {
-            Debug.Log("JumpEnter");
-            Context.ChangeParentRayObject();
+            orca = Context.orcaModel.transform;
+            Jump(orca.localPosition);
+            Rotate();
+            //Context.ChangeParentRayObject();
         }
         protected internal override void Update()
         {
-            Debug.Log("JumpUpdate");
+            //Move();
+
             if (Input.GetKeyDown(KeyCode.A))
             {
-                stateMachine.SendEvent((int)StateEventId.Idle);
+                
+                //stateMachine.SendEvent((int)StateEventId.Idle);
             }
         }
         protected internal override void Exit()
         {
-            Debug.Log("JumpExit");
         }
     }
 }
