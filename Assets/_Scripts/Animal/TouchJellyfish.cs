58c1b136cffc7204dc1ae6490d38b2c0c7d0aeaa
ebonyCitizen <coco_coco87@hotmail.com>
2019-11-14 12:21:57 +0900

    Stage1草のTerrain

---
+++ Assets/_Scripts/Animal/TouchJellyfish.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TouchJellyfish : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject eyeEffect;
+    [SerializeField]
+    private GameObject handEffect;
+
+    private GameObject cameraEye;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraEye = Object.FindObjectOfType<RayFromCamera>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Hand"))
+        {
+            ElectricShock(collision.gameObject);
+        }
+    }
+
+    private void ElectricShock(GameObject hand)
+    {
+        Sequence s = DOTween.Sequence();
+        s.AppendCallback(() => hand.GetComponentInChildren<SkinnedMeshRenderer>().material.EnableKeyword("_EMISSION"))
+            .AppendCallback(() => Instantiate(handEffect, hand.transform))
+            .AppendCallback(() => Instantiate(eyeEffect, cameraEye.transform))
+            .AppendInterval(0.6f)
+            .AppendCallback(() => hand.GetComponentInChildren<SkinnedMeshRenderer>().material.DisableKeyword("_EMISSION"));
+        s.Play();
+    }
+}
--- Assets/_Scripts/State/State/ClickState.cs
+++ Assets/_Scripts/State/State/ClickState.cs
@@ -3,7 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using IceMilkTea.Core;
 
-public class ClickState : ImtStateMachine<OrcaState>.State
+public partial class OrcaState
 {
+    public class ClickState : ImtStateMachine<OrcaState>.State
+    {
+        private Transform orca;
+        private Transform rayObject;
 
+        private Transform rot;
+
+        protected internal override void Enter()
+        {
+            Context.ChangeParentRayObject();
+            orca = Context.orcaModel.transform;
+            rayObject = Context.rayObject.transform;
+
+            rot = Context.idleRotation;
+            Debug.Log("ClickEnter");
+        }
+        protected internal override void Update()
+        {
+            
+        }
+        protected internal override void Exit()
+        {
+            
+        }
+    }
 }
