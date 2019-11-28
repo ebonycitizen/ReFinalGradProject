71bf37159a052b63d28b268c533fed65390d7ed0
huyup <irrsfunmaker@gmail.com>
2019-11-28 20:01:29 +0900

    add ペンギンのモデルを更新

---
+++ Assets/_Scripts/Enemy/PenguinTriggerCtr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinTriggerCtr : MonoBehaviour
+{
+    [SerializeField]
+    private List<PenguinAnimatorCtr> penguinAnimators = new List<PenguinAnimatorCtr>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Dolly"))
+        {
+            penguinAnimators.ForEach(x =>
+            {
+                x.PlayNextAnimation("Shout");
+            });
+        }
+
+    }
+}
