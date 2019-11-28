cef8ab9c6912224758e7440150058e33158853d4
km323 <miyakm323@gmail.com>
2019-11-28 13:33:49 +0900

    Coral配置

---
+++ Assets/DolphinState/DolphinState.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IceMilkTea.Core;
+using BehaviorDesigner.Runtime;
+
+public partial class DolphinState : MonoBehaviour
+{
+    private enum StateEventId
+    {
+        None,
+    }
+
+    private ImtStateMachine<DolphinState> stateMachine;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stateMachine = new ImtStateMachine<DolphinState>(this);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
