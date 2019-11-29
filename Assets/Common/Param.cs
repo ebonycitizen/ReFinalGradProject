af198936ec4971d2cde6de2434eb05020e23505a
ebonyCitizen <coco_coco87@hotmail.com>
2019-11-30 07:12:07 +0900

    boid ecs

---
+++ Assets/Common/Param.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace BoidECS
+{
+
+[CreateAssetMenu(menuName = "BoidECS/Param")]
+public class Param : ScriptableObject
+{
+    public float initSpeed = 2f;
+    public float minSpeed = 2f;
+    public float maxSpeed = 5f;
+    public float neighborDistance = 1f;
+    public float neighborFov = 90f;
+    public float separationWeight = 5f;
+    public Vector3 wallScale = Vector3.one * 5f;
+    public float wallDistance = 3f;
+    public float wallWeight = 1f;
+    public float alignmentWeight = 2f;
+    public float cohesionWeight = 3f;
+    public float targetWeight = 3f;
+}
+
+}
