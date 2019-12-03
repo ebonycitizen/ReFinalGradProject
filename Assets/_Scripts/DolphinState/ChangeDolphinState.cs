9be438adbfb4b0f70ad752ef33d136700609c29e
km323 <miyakm323@gmail.com>
2019-12-04 04:47:28 +0900

    Coral

--- Assets/_Scripts/DolphinState/ChangeDolphinState.cs
+++ Assets/_Scripts/DolphinState/ChangeDolphinState.cs
@@ -6,12 +6,14 @@ public class ChangeDolphinState : MonoBehaviour
 {
     [SerializeField]
     private DolphinState dolphinState;
+    [SerializeField]
+    private GameObject sendObjct=null;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Dolly"))
         {
-            bool hasChangeState = dolphinState.ChangeState(gameObject.tag, null);
+            bool hasChangeState = dolphinState.ChangeState(gameObject.tag, sendObjct);
             if (!hasChangeState)
                 return;
             Destroy(gameObject);
