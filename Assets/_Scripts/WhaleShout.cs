e97bbbd24ad97cc7c4d8750630afc8905a6bf0c6
ebonyCitizen <coco_coco87@hotmail.com>
2020-01-14 17:42:05 +0900

    bug修正

---
+++ Assets/_Scripts/WhaleShout.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+using Random = UnityEngine.Random;
+
+public class WhaleShout : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject whales;
+
+    private Animator[] animators;
+
+    [SerializeField]
+    private float value = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animators = whales.GetComponentsInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Dolly"))
+        {
+            foreach(var a in animators)
+            {
+                value = Random.value*value/*0~1*value */;
+                Observable.Timer(TimeSpan.FromSeconds(value))
+                    .Subscribe(_ =>
+                    {
+                        a.SetTrigger("Shout");
+                    }).AddTo(this);
+            }
+        }
+    }
+}
