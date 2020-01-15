e3ea4226adf986d9dffb6926cf2342c13896a6ea
km323 <miyakm323@gmail.com>
2020-01-15 20:29:13 +0900

    HeadCariblator

---
+++ Assets/HeadCaribrator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadCaribrator : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cameraRig;
+    [SerializeField]
+    private Transform eyePos;
+    [SerializeField]
+    private Transform offsetEyePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Callibrate();
+        }
+    }
+
+    public void Callibrate()
+    {
+        var gapPos = offsetEyePos.position - eyePos.position;
+        cameraRig.position += gapPos;
+    }
+}
