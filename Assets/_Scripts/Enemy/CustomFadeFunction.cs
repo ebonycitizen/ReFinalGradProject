82cf62a26a173321f9eb267b49ad55ec43ceee84
huyup <irrsfunmaker@gmail.com>
2020-01-16 18:36:14 +0900

    add スタッフロール仮

---
+++ Assets/_Scripts/Enemy/CustomFadeFunction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using System;
+using UnityEngine;
+using DG.Tweening;
+using UniRx;
+public class CustomFadeFunction : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI m_textMeshPro;
+
+
+    [SerializeField]
+    private float m_waitTime = 0;
+
+    [SerializeField]
+    private float m_nextTime = 0;
+
+    [SerializeField]
+    private float m_duration = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Observable.Timer(TimeSpan.FromSeconds(m_waitTime)).
+            Subscribe(_ =>
+            {
+                m_textMeshPro.DOFade(1, m_duration+0.5f);
+            }).AddTo(this);
+
+        Observable.Timer(TimeSpan.FromSeconds(m_nextTime)).
+            Subscribe(_ =>
+            {
+                m_textMeshPro.DOFade(0, m_duration);
+            }).AddTo(this);
+
+    }
+}
---
+++ Assets/_Scripts/Enemy/CustomFadeFunctionForModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using System;
+using UnityEngine;
+using DG.Tweening;
+using UniRx;
+public class CustomFadeFunctionForModel : MonoBehaviour
+{
+    [SerializeField]
+    private Material m_material;
+
+
+    [SerializeField]
+    private float m_waitTime = 0;
+
+    [SerializeField]
+    private float m_nextTime = 0;
+
+    [SerializeField]
+    private float m_duration = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Observable.Timer(TimeSpan.FromSeconds(m_waitTime)).
+            Subscribe(_ =>
+            {
+                m_material.DOFade(1, m_duration);
+
+            }).AddTo(this);
+
+        Observable.Timer(TimeSpan.FromSeconds(m_nextTime)).
+            Subscribe(_ =>
+            {
+                m_material.DOFade(0, m_duration);
+
+            }).AddTo(this);
+
+    }
+}
